6a8676bfe82843c7f32c4f5fc6f85fd7b3d05666
Robert Witt <robert.witt@zalando.de>
2019-03-16 11:41:22 +0100

    Add Aux.js to wrap adjacent JSX elements.

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -6,7 +6,7 @@ const cockpit = props => {
     console.log('[Cockpit.js] useEffect');
     // HTTP request...
     setTimeout(() => {
-      alert('Saved data to cloud!');
+      // alert('Saved data to cloud!');
     }, 1000);
     return () => {
       console.log('[Cockpit.js] cleanup work in useEffect');
